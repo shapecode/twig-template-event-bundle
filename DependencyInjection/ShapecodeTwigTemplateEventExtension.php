99d33d2a9f9fba3c5d8393023bb2e6456651281f
Nikita Loges <n.loges@tenolo.de>
2015-06-23 15:16:22 +0200

    remove configuration class

--- DependencyInjection/ShapecodeTwigTemplateEventExtension.php
+++ DependencyInjection/ShapecodeTwigTemplateEventExtension.php
@@ -2,10 +2,10 @@
 
 namespace Shapecode\Bundle\TwigTemplateEventBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Class ShapecodeTwigTemplateEventExtension
@@ -20,9 +20,6 @@ class ShapecodeTwigTemplateEventExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
