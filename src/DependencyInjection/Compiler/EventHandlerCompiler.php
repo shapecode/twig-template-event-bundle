ffd28ada5089d21960bdfef8b141b3a1a39cf1b9
fyrye <fyrye@users.noreply.github.com>
2020-09-09 01:09:53 -0400

    Rename shapecode_twig_template_event.handler_manager
    Fix #3 pt 2

--- src/DependencyInjection/Compiler/EventHandlerCompiler.php
+++ src/DependencyInjection/Compiler/EventHandlerCompiler.php
@@ -7,6 +7,7 @@ namespace Shapecode\Bundle\TwigTemplateEventBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
+use Shapecode\Bundle\TwigTemplateEventBundle\Manager\HandlerManager;
 
 final class EventHandlerCompiler implements CompilerPassInterface
 {
@@ -15,7 +16,7 @@ final class EventHandlerCompiler implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container): void
     {
-        $manager = $container->getDefinition('shapecode_twig_template_event.handler_manager');
+        $manager = $container->getDefinition(HandlerManager::class);
         $tags    = $container->findTaggedServiceIds('shapecode_twig_template_event.handler');
 
         foreach ($tags as $id => $config) {
